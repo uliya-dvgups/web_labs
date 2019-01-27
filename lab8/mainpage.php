4c3b20a36adfd746ad9758768b7c17678e6a47fe
Uliya Martynova <uliya_dvgups@inbox.ru>
2019-01-27 09:58:53 +0300

    Лабораторная работа №8. Начало

---
+++ lab8/mainpage.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>WEB PHP</title>
+  </head>
+
+  <body>
+        <h3>Решение задач веб-разработки на стороне сервера на языке PHP</h3>
+        <p><a href="http://uliya.labs/lab8/my_form.php">Форма</a></p>
+        <p><a href="http://uliya.labs/lab8/my_site.php">Сайт</a></p>
+        <p><a href="http://uliya.labs/">Главная страница</a></p>
+  </body>
+</html>
