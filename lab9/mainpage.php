48eb55e2e01a33d20d4f243f9b5fa1901dcf127b
Uliya Martynova <uliya_dvgups@inbox.ru>
2019-01-27 10:02:26 +0300

    Лабораторная работа №9. Начало

---
+++ lab9/mainpage.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>WEB PHP Взаимодействие PHP и MySQL</title>
+  </head>
+
+  <body>
+        <h3>Работа с БД MySQL через PHP</h3>
+        <p><a href="http://uliya.labs/lab9/all_note.php">Посмотреть все записи</a></p>
+        <p><a href="http://uliya.labs/lab9/filter_note.php">Вывод записей по одному из полей</a></p>
+        <p><a href="http://uliya.labs/lab9/add_note.php">Создать новую запись</a></p>
+        <p><a href="http://uliya.labs/lab9/edit_note.php">Изменить существующую запись</a></p>
+        <p><a href="http://uliya.labs/lab9/del_note.php">Удалить запись</a></p>
+        <p><a href="http://uliya.labs/">Главная страница</a></p>
+  </body>
+</html>
