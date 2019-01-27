4cebe11aad60761efda5072b873e457f89a05d7b
Uliya Martynova <uliya_dvgups@inbox.ru>
2019-01-27 09:55:53 +0300

    Лабораторная работа №11

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>WEB PHP</title>
+  </head>
+
+  <body>
+        <p><a href="http://uliya.labs/lab8/mainpage.php">Лабораторная работа №8</a></p>
+        <p><a href="http://uliya.labs/lab9/mainpage.php">Лабораторная работа №9</a></p>
+        <p><a href="http://uliya.labs/lab10/mainpage.php">Лабораторная работа №10</a></p>
+  </body>
+</html>
